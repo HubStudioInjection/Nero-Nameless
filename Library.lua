42fe0ffc4d152369cffe7ca6d6c9d9ba1ce81627
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-26 15:00:06 +0200

    Library.lua

--- Library.lua
+++ Library.lua
@@ -209,6 +209,25 @@ function Kavo.CreateLib(kavName, themeList)
     local Pages = Instance.new("Folder")
     local infoContainer = Instance.new("Frame")
 
+local Toggle = Instance.new("TextButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0, 50, 0,50)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Ui"
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
+Toggle.TextSize = 16.000
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+
+local Corner = Instance.new("UICorner")
+Corner.Name = "Corner"
+Corner.Parent = Toggle
+	
     local blurFrame = Instance.new("Frame")
 
     Kavo:DraggingEnabled(MainHeader, Main)
