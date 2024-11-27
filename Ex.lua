f73cf059942abb74d80f158581de7b535fffe61e
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-27 22:31:27 +0200

    Ex.lua

--- Ex.lua
+++ Ex.lua
@@ -1,6 +1,1 @@
---Hi Lua Obfuscator!
-if game.PlaceId == 3623096087 then
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/928491GB/ssssssse3w/refs/heads/main/Cult.lua"), true))()
-else
-	game.Players.LocalPlayer:Kick("Join Supported Game.")
-end
+loadstring(game:HttpGet((""), true))()
